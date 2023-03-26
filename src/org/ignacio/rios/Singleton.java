1132fd497d0035f407b5efe9a453e4ea0327b070
igrios1979 <ignarios1@hotmail.com>
2023-03-26 19:31:14 -0300

    Primer patron singleton

--- src/org/ignacio/rios/Singleton.java
+++ src/org/ignacio/rios/Singleton.java
@@ -5,18 +5,9 @@ public class Singleton {
     public static void main(String[] args) {
         
         for (int i = 0; i<100;i++){
-            
             ConexionBDSingleton v = ConexionBDSingleton.getInstance();
-
             System.out.println("v = " + v);
-            
-            
-        }
-        
-        
-        
-        
+                    }
+
     }
-
-
 }
